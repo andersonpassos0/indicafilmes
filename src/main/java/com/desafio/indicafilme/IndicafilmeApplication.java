b7bdf5a72f129d7ca54fc3e32a90dc404a77ae83
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-07-07 21:48:14 -0300

    git commit

---
+++ src/main/java/com/desafio/indicafilme/IndicafilmeApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.indicafilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicafilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicafilmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/indicafilme/IndicafilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.indicafilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicafilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
