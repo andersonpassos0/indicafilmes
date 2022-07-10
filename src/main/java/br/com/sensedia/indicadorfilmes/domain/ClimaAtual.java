d464a9c825270a339c6750ee6d49a5b2f3e24e84
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-07-09 23:02:23 -0300

    Implementado ClimaAtualAPI

---
+++ src/main/java/br/com/sensedia/indicadorfilmes/domain/ClimaAtual.java
@@ -0,0 +1,11 @@
+package br.com.sensedia.indicadorfilmes.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClimaAtual {
+	private Integer cep = 45807000;
+
+}
