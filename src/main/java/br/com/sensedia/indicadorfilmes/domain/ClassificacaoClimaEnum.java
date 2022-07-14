95f68d98aa17bb228a43a6ebcda32a37fe552c94
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-07-14 01:02:41 -0300

    Implementado IndicadorDeFilmeService

--- src/main/java/br/com/sensedia/indicadorfilmes/domain/ClassificacaoClimaEnum.java
+++ src/main/java/br/com/sensedia/indicadorfilmes/domain/ClassificacaoClimaEnum.java
@@ -1,8 +1,46 @@
+//package br.com.sensedia.indicadorfilmes.domain;
+//
+//import java.util.stream.Stream;
+//
+//import lombok.Getter;
+//
+//@Getter
+//public enum ClassificacaoClimaEnum {
+//
+//	FRIO_GLACIAL(-20,-1),
+//	FRIO(0,19),
+//	AGRADAVEL(20,34),
+//	QUENTE(35,39),
+//	INFERNO(40,60);
+//	
+//	private Integer min;
+//	private Integer max;
+//	
+//	ClassificacaoClimaEnum(Integer min, Integer max){
+//	this.min = min;
+//	this.max = max;
+//	}
+//	
+//	public static String buscaClassificacaoPorTemperatura(Integer temperatura, Integer min, Integer max) {
+//		String classificacaoClima = ClassificacaoClimaEnum.stream()
+//			.filter(c -> c.getMin() >= min && c.getMax() < max)
+//			.toString();
+//		return classificacaoClima;
+//	}
+//	
+//	
+//	public static Stream<ClassificacaoClimaEnum> stream(){
+//		return Stream.of(ClassificacaoClimaEnum.values());
+//	}
+//	
+//	
+//}
+
 package br.com.sensedia.indicadorfilmes.domain;
 
-import java.util.stream.Stream;
-
 import lombok.Getter;
+
+import java.util.stream.Stream;
 
 @Getter
 public enum ClassificacaoClimaEnum {
@@ -20,18 +58,13 @@ public enum ClassificacaoClimaEnum {
 	this.min = min;
 	this.max = max;
 	}
-	
-	public static String buscaClassificacaoPorTemperatura(Integer temperatura, Integer min, Integer max) {
-		String classificacaoClima = ClassificacaoClimaEnum.stream()
-			.filter(c -> c.getMin() >= min && c.getMax() < max)
-			.toString();
-		return classificacaoClima;
+
+	public static ClassificacaoClimaEnum buscaClassificacaoPorTemperatura(Integer temperatura) {
+		return Stream.of(ClassificacaoClimaEnum.values())
+				.filter(c -> temperatura >= c.getMin() && temperatura <= c.getMax())
+				.findFirst()
+				.orElseThrow();
 	}
-	
-	
-	public static Stream<ClassificacaoClimaEnum> stream(){
-		return Stream.of(ClassificacaoClimaEnum.values());
-	}
-	
 	
 }
+
