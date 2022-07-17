66e6b5f7dbf5567c7a57c63f10987f41f5ac657c
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-07-17 13:17:17 -0300

    Adiciona Feign

--- src/main/java/br/com/sensedia/indicadorfilmes/categoriafilme/domain/CategoriaFilme.java
+++ src/main/java/br/com/sensedia/indicadorfilmes/categoriafilme/domain/CategoriaFilme.java
@@ -1,8 +1,4 @@
 package br.com.sensedia.indicadorfilmes.categoriafilme.domain;
 
-import org.springframework.data.annotation.Id;
-
 public class CategoriaFilme {
-	@Id
-	private Integer id;
 }
--- src/main/java/br/com/sensedia/indicadorfilmes/filme/infra/FilmeClientInfraAPI.java
+++ src/main/java/br/com/sensedia/indicadorfilmes/filme/infra/FilmeClientInfraAPI.java
@@ -2,10 +2,13 @@ package br.com.sensedia.indicadorfilmes.filme.infra;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import br.com.sensedia.indicadorfilmes.categoriafilme.domain.CategoriaFilme;
 import br.com.sensedia.indicadorfilmes.filme.application.service.FilmeClientAPI;
 import br.com.sensedia.indicadorfilmes.filme.domain.Filme;
 
+@Component
 public class FilmeClientInfraAPI implements FilmeClientAPI {
 
 	@Override
