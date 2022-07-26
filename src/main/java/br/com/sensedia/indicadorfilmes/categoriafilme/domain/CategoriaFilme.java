ae92e18144e6baf0cf077dd1610b19a2d67700a2
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-07-26 01:02:31 -0300

    Mapeia entidade Filme e CategoriaFilme

--- src/main/java/br/com/sensedia/indicadorfilmes/categoriafilme/domain/CategoriaFilme.java
+++ src/main/java/br/com/sensedia/indicadorfilmes/categoriafilme/domain/CategoriaFilme.java
@@ -1,5 +1,8 @@
 package br.com.sensedia.indicadorfilmes.categoriafilme.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import br.com.sensedia.indicadorfilmes.clima.domain.ClassificacaoClimaEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,8 +13,9 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class CategoriaFilme {
-	private Integer id;
+	@Id
 	private CategoriaFilmeEnum categoria;
 	private ClassificacaoClimaEnum classificacaoClima;
 
--- src/main/java/br/com/sensedia/indicadorfilmes/filme/domain/Filme.java
+++ src/main/java/br/com/sensedia/indicadorfilmes/filme/domain/Filme.java
@@ -3,13 +3,24 @@ package br.com.sensedia.indicadorfilmes.filme.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import br.com.sensedia.indicadorfilmes.categoriafilme.domain.CategoriaFilmeEnum;
 import br.com.sensedia.indicadorfilmes.filme.infra.Movie;
 import br.com.sensedia.indicadorfilmes.filme.infra.NowPlayingMoviesResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Filme {
+	@Id
 	private String titulo;
 	private CategoriaFilmeEnum categoria;
 
